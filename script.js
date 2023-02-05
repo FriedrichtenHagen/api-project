c4f3776c2dcdeea323b49d2d916def6ff9ec75db
Friedrich <friedrich.ten.hagen@googlemail.com>
2023-02-05 13:25:05 +0100

    rewrite code for pokemon quiz and display description of right pokemon

--- script.js
+++ script.js
@@ -2,107 +2,113 @@
 // https://pokeapi.co/
 // https://developer.spotify.com/documentation/web-api/
 
-const sprite = document.querySelector(".sprite")
-// three states of promises: pending, resolved, rejected
+const sprites = document.querySelector(".sprites")
 
-const searchButton = document.querySelector("#searchButton")
-searchButton.addEventListener("click", () => {
-	// read query from input element
-	let query = document.querySelector("#pokeSearch").value.toLowerCase()
-	console.log("search query: " + query)
 
-	makeSearchrequest(query)
-	makeTypeRequest(query)
-})
+function generateRandomPokemon(){
+	// generate random answer Pokemon
+	let rightPokemon = 1
+
+	// generate 8 other random Pokemon
+	let wrongPokemon = 1
+}
+
 
 
 // using async and await syntax
-async function makeSearchrequest(query){
+async function getPokemon(){
 	
-	// fetch data from api
+	// query must be ID of that Pokemon
+	let query = "pikachu" 
+
+	// fetch pokemonData from api
 	const response = await fetch(`https://pokeapi.co/api/v2/pokemon/${query}`)
 	// fetch returns a promise
 	try{
 		console.log(response)
-		let data = await response.json()
-		console.log(data)
-		displaySprite(data)
-		displayName(data)
-		fetchDescriptions(data)	
+		// transform response into json
+		let pokemonData = await response.json()
+		console.log(pokemonData)
+
+		// get description for answer
+		fetchDescription(pokemonData)	
+
+		// display the description of answer pokemon
+		displayDescription(pokemonData)
+
+		// display all 9 pokemon in DOM
+		displaySprites(pokemonData)
+
+
 	} catch (error){
 		console.log("MY ERROR: " +error)
 	}
 }
 
-async function fetchDescriptions(data){
+async function fetchDescription(pokemonData){
 	try{
-		const pokemonUrl = data.species.url
+		const pokemonUrl = pokemonData.species.url
 		const response = await fetch(pokemonUrl)
-		let pokemonData = await response.json()
-		console.log(pokemonData)
-		console.log(pokemonData.genera[7].genus) // english poke type
+		let pokemonSpeciesJson = await response.json()
+		console.log(pokemonSpeciesJson)
 		
-		console.log(pokemonData.flavor_text_entries[0].flavor_text)
+		displayDescription(pokemonSpeciesJson.flavor_text_entries[0].flavor_text)
+
 	} catch(error){
 		console.log("Pokemon Retrieval error: " + error)
 	}	
 }
 
-
-
-
-async function makeTypeRequest(query){
-
-	const pokeTypeColors = {
-		"Psi Pokémon" : "purple",
-
-	}
-
-	// fetch data from api
-	const response = await fetch(`https://pokeapi.co/api/v2/type/${query}/`)
-	// fetch returns a promise
-	try{
-		console.log(response)
-		let data = await response.json()
-		console.log(data)
-		
-	} catch (error){
-		console.log("MY ERROR: " +error)
-	}
-
-
-
+function displayDescription(speciesDescription){
+	const name = document.querySelector(".description")
+	name.textContent = speciesDescription
 }
 
-
-
-
-function displayName(data){
-	const name = document.querySelector(".name")
-	name.textContent = data.name
-}
 
 
 // display the pokemon sprites in the DOM
-function displaySprite(data){
-	let spriteArray = Object.keys(data.sprites)
+function displaySprites(pokemonData){
+	let spriteArray = Object.keys(pokemonData.sprites)
 	for(let key of spriteArray){
 		// only display front facing image
 		if(key=== "front_default"){
-			if(data.sprites[key]!==null&&typeof(data.sprites[key])!== "object"){
+			if(pokemonData.sprites[key]!==null&&typeof(pokemonData.sprites[key])!== "object"){
 				let newSpriteImage = document.createElement("img")
-				newSpriteImage.src = `${data.sprites[key]}`
-				sprite.append(newSpriteImage)
+				newSpriteImage.src = `${pokemonData.sprites[key]}`
+				sprites.append(newSpriteImage)
 			}
 		}
 	}
 }
 
+
+async function makeTypeRequest(query){
+	const pokeTypeColors = {
+		"Psi Pokémon" : "purple",
+		"Flame Pokémon": "red"
+
+	}
+	// fetch pokemonData from api
+	const response = await fetch(`https://pokeapi.co/api/v2/type/ground/`)
+	// fetch returns a promise
+	try{
+		console.log(response)
+		let pokemonData = await response.json()
+		console.log(pokemonData)
+		
+	} catch (error){
+		console.log("MY ERROR: " +error)
+	}
+}
+
+getPokemon()
+
+
 /*
 Pokemon quiz
 generate random (winning pokemon)
-fetch data.species.url for the description
-data.game_indices limit to old school pokemon
+fetch pokemonData.species.url for the description
+pokemonData.game_indices limit to old school pokemon
 
 display four option button with 3 random pokemon
 	and the winning pokemon that matches the description
